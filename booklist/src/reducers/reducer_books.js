c6d2b4b92a991e9f79ea4cec8aa2216052202fb7
Christian262 <christian.lautenschleger@gmail.com>
2017-09-20 14:08:12 -0400

    first commit

---
+++ booklist/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        { title: 'JavaScript: The Good Parts', pages: 120 },
+        { title: 'Jurassic Park', pages: 400},
+        { title: 'Flow', pages: 250},
+        { title: 'Daniels\' Running Formula', pages: 325}
+    ]
+}
